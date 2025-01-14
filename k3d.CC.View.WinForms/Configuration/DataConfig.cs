a46e1d55037f20cd505521912dba5b13aad9357a
Constantin Ushkov <konst3d@gmail.com>
2025-01-14 16:11:44 +0200

    Implemented configuration reading from app.config.

--- k3d.CC.View.WinForms/Configuration/DataConfig.cs
+++ k3d.CC.View.WinForms/Configuration/DataConfig.cs
@@ -1,13 +1,24 @@
-﻿using IDataConfiguration = k3d.CC.Data.Impl.FS.IConfiguration;
+﻿using System.Configuration;
+using IDataConfiguration = k3d.CC.Data.Impl.FS.IConfiguration;
 
 namespace k3d.CC.View.WinForms.Configuration
 {
     internal class DataConfig : IDataConfiguration
     {
-        public string BaseFolder => throw new NotImplementedException();
+        private static class Constants
+        {
+            public const string Prefix = "Data";
+            public const string BaseFolder = "BaseFolder";
+        }
+
+        public string BaseFolder { get; private set; }
+
+        public DataConfig(string baseFolder)
+        {
+            BaseFolder = baseFolder;
+        }
 
         public static IDataConfiguration FromAppConfig()
-        {
-        }
+            => new DataConfig(ConfigurationManager.AppSettings[$"{Constants.Prefix}.{Constants.BaseFolder}"]!);
     }
 }
--- k3d.CC.View.WinForms/Configuration/IApplicationConfiguration.cs
+++ k3d.CC.View.WinForms/Configuration/IApplicationConfiguration.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using IDataConfiguration = k3d.CC.Data.Impl.FS.IConfiguration;
+﻿using IDataConfiguration = k3d.CC.Data.Impl.FS.IConfiguration;
 
 namespace k3d.CC.View.WinForms.Configuration
 {
